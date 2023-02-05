682fed05178ba8f599fb6f803614fae528eddf1e
Your Name <danilka2330@gmail.com>
2023-02-05 16:47:18 +0300

    first commit

---
+++ cmake-build-debug/CMakeFiles/lab5.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/Admin/CLionProjects/Director-s-Interface/Director.cpp" "CMakeFiles/lab5.dir/Director.cpp.obj" "gcc" "CMakeFiles/lab5.dir/Director.cpp.obj.d"
+  "C:/Users/Admin/CLionProjects/Director-s-Interface/Employee.cpp" "CMakeFiles/lab5.dir/Employee.cpp.obj" "gcc" "CMakeFiles/lab5.dir/Employee.cpp.obj.d"
+  "C:/Users/Admin/CLionProjects/Director-s-Interface/FIO.cpp" "CMakeFiles/lab5.dir/FIO.cpp.obj" "gcc" "CMakeFiles/lab5.dir/FIO.cpp.obj.d"
+  "C:/Users/Admin/CLionProjects/Director-s-Interface/Salary.cpp" "CMakeFiles/lab5.dir/Salary.cpp.obj" "gcc" "CMakeFiles/lab5.dir/Salary.cpp.obj.d"
+  "C:/Users/Admin/CLionProjects/Director-s-Interface/accountant.cpp" "CMakeFiles/lab5.dir/accountant.cpp.obj" "gcc" "CMakeFiles/lab5.dir/accountant.cpp.obj.d"
+  "C:/Users/Admin/CLionProjects/Director-s-Interface/admin.cpp" "CMakeFiles/lab5.dir/admin.cpp.obj" "gcc" "CMakeFiles/lab5.dir/admin.cpp.obj.d"
+  "C:/Users/Admin/CLionProjects/Director-s-Interface/main.cpp" "CMakeFiles/lab5.dir/main.cpp.obj" "gcc" "CMakeFiles/lab5.dir/main.cpp.obj.d"
+  "C:/Users/Admin/CLionProjects/Director-s-Interface/security.cpp" "CMakeFiles/lab5.dir/security.cpp.obj" "gcc" "CMakeFiles/lab5.dir/security.cpp.obj.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/lab5.dir/cmake_clean.cmake
@@ -0,0 +1,27 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lab5.dir/Director.cpp.obj"
+  "CMakeFiles/lab5.dir/Director.cpp.obj.d"
+  "CMakeFiles/lab5.dir/Employee.cpp.obj"
+  "CMakeFiles/lab5.dir/Employee.cpp.obj.d"
+  "CMakeFiles/lab5.dir/FIO.cpp.obj"
+  "CMakeFiles/lab5.dir/FIO.cpp.obj.d"
+  "CMakeFiles/lab5.dir/Salary.cpp.obj"
+  "CMakeFiles/lab5.dir/Salary.cpp.obj.d"
+  "CMakeFiles/lab5.dir/accountant.cpp.obj"
+  "CMakeFiles/lab5.dir/accountant.cpp.obj.d"
+  "CMakeFiles/lab5.dir/admin.cpp.obj"
+  "CMakeFiles/lab5.dir/admin.cpp.obj.d"
+  "CMakeFiles/lab5.dir/main.cpp.obj"
+  "CMakeFiles/lab5.dir/main.cpp.obj.d"
+  "CMakeFiles/lab5.dir/security.cpp.obj"
+  "CMakeFiles/lab5.dir/security.cpp.obj.d"
+  "lab5.exe"
+  "lab5.exe.manifest"
+  "lab5.pdb"
+  "liblab5.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/lab5.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
